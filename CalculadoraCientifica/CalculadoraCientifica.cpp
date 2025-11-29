d270d89cc218200ae0944f6395b349c4574f85dc
Mictlah <112373315+Mictlah@users.noreply.github.com>
2025-11-29 11:14:28 -0700

    Funciones básicas
    Suma, resta y multiplicación

--- CalculadoraCientifica/CalculadoraCientifica.cpp
+++ CalculadoraCientifica/CalculadoraCientifica.cpp
@@ -3,6 +3,21 @@
 #include <cmath>
 using namespace std;
 
+double suma(double a, double b)
+{
+    return a + b;
+}
+
+double resta(double a, double b)
+{
+    return a - b;
+}
+
+double multiplicacion(double a, double b)
+{
+    return a * b;
+}
+
 int main(){
     return 0;
 }
