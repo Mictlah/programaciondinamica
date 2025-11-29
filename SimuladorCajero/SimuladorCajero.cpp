f224837a6c4a5312bc7b5a706fecfbcee5c23e14
Mictlah <112373315+Mictlah@users.noreply.github.com>
2025-11-29 10:33:13 -0700

    Clase Cuenta
    Se agrega una de las clases importantes, la clase cuenta, esta tiene todos los atributos necesarios, para poder retirar, depositar, consultar.

--- SimuladorCajero/SimuladorCajero.cpp
+++ SimuladorCajero/SimuladorCajero.cpp
@@ -5,6 +5,42 @@
 
 using namespace std;
 
+class Cuenta{
+    private: 
+        string numeroCuenta;
+        string pin;
+        double saldo;
+
+    public:
+        Cuenta(string num, string p, double saldoInicial)
+            : numeroCuenta(num), pin(p), saldo(saldoInicial){}
+        
+            bool verificarPIN(string pinIngresado){
+                return pin == pinIngresado;
+            }
+
+        double consultarSaldo(){
+            return saldo;
+        }
+
+        bool retirar(double cantidad){
+            if (cantidad <= 0){
+                cout << "Error: Cantidad Invalida\n";
+                return false;
+            }
+            if (cantidad > saldo){
+                cout << "Error: Fondos Insuficientes\n";
+                return false;
+            }
+            saldo -= cantidad;
+            return true;
+        }
+
+        string obtenerNumeroCuenta(){
+            return numeroCuenta;
+        }
+};
+
 int main(){
     return 0;
 }
