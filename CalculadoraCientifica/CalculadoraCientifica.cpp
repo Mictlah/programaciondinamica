708c8ff7f7659a1979e8ac64488c22f47b55f1f1
Mictlah <112373315+Mictlah@users.noreply.github.com>
2025-11-29 11:23:46 -0700

    Funcion principal

--- CalculadoraCientifica/CalculadoraCientifica.cpp
+++ CalculadoraCientifica/CalculadoraCientifica.cpp
@@ -27,13 +27,13 @@ double division(double a, double b)
 
 int factorial(int n) {
     if (n < 0)
-        throw invalid_argument("Factorial de numero negativo no definido.")
+        throw invalid_argument("Factorial de numero negativo no definido.");
     if (n == 0 || n == 1)
         return 1;
     return n * factorial(n-1);
 }
 
-double potencia(dobuel base, int exponente = 2){
+double potencia(double base, int exponente = 2){
     return pow(base, exponente);
 }
 
@@ -48,5 +48,59 @@ double raiz(double x){
 }
 
 int main(){
+    int opcion;
+    double n1, n2;
+    cout << "CALCULADORA MULTIFUNCION\n";
+    cout << "1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n5. Factorial\n6. Potencia\n7. Raoz cuadrada\n";
+    cout << "Seleccione una opcion: ";
+    cin >> opcion;
+
+    try
+    {
+        switch (opcion)
+        {
+        case 1:
+            cout << "Ingrese dos numeros: ";
+            cin >> n1 >> n2;
+            cout << "Resultado: " << suma(n1, n2) << endl;
+            break;
+        case 2:
+            cout << "Ingrese dos numeros: ";
+            cin >> n1 >> n2;
+            cout << "Resultado: " << resta(n1, n2) << endl;
+            break;
+        case 3:
+            cout << "Ingrese dos numeros: ";
+            cin >> n1 >> n2;
+            cout << "Resultado: " << multiplicacion(n1, n2) << endl;
+            break;
+        case 4:
+            cout << "Ingrese dos numeros: ";
+            cin >> n1 >> n2;
+            cout << "Resultado: " << division(n1, n2) << endl;
+            break;
+        case 5:
+            cout << "Ingrese un numero: ";
+            cin >> n1;
+            cout << "Resultado: " << factorial((int)n1) << endl;
+            break;
+        case 6:
+            cout << "Ingrese base y exponente: ";
+            cin >> n1 >> n2;
+            cout << "Resultado: " << potencia(n1, (int)n2) << endl;
+            break;
+        case 7:
+            cout << "Ingrese un número: ";
+            cin >> n1;
+            cout << "Resultado: " << raiz(n1) << endl;
+            break;
+        default:
+            cout << "Opción no válida.\n";
+        }
+    }
+    catch (const exception &e)
+    {
+        cout << "Error: " << e.what() << endl;
+    }
     return 0;
 }
